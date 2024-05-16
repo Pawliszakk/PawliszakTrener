fdd92c1c9060ed214a434ad827b201495b01eb1d
Pawliszakk <oskar.pawliszak1@o2.pl>
2024-05-16 15:16:02 +0200

    added about sections

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Navbar from '@/components/layout/nav/Navbar';
+import Navbar from '@/components/layout/Nav/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
---
+++ components/About/About.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image';
+import classes from './About.module.scss';
+import { SiInstagram } from 'react-icons/si';
+import { FaEnvelope, FaSquareFacebook } from 'react-icons/fa6';
+const About = () => {
+	return (
+		<section className={classes.about} id="o-mnie">
+			<div className={classes.photo}>
+				<Image
+					src="/assets/trener/alfa.png"
+					width={400}
+					height={400}
+					alt="Zdjęcie trenera Pawliszaka"
+				/>
+			</div>
+			<div className={classes.content}>
+				<h3>
+					Nazywam się <span>Oskar Pawliszak</span>
+				</h3>
+				<p>
+					Moja przygoda ze sportem rozpoczęła się ponad 10 lat temu a od ponad 5
+					lat zajmuje się sportami siłowymi. Na początku trenowałem trójbój
+					siłowy lecz wyspecjalizowałem się w wyciskaniu leżąc i temu oddałem
+					się najbardziej jak dotąd kończąc na wyniku <span>170 kg</span> na
+					ławce poziomej.{' '}
+					<a href="https://www.instagram.com/pawliszakk/">Sprawdź instagram</a>
+				</p>
+				<p>
+					Od ponad 3 lat skutecznie prowadzę ludzi, treningi personalne i
+					rozpisuje plany treningowe aby każdy podopieczny osiągnął wyznaczony
+					sobie cel w jak najbardziej efektywny i bezpieczny sposób.{' '}
+				</p>
+
+				<div className={classes.socials}>
+					<div className={classes.social}>
+						<a href="mailto:oskar.pawliszak1@o2.pl">
+							<FaEnvelope />
+						</a>
+					</div>
+					<div className={classes.social}>
+						<a href="https://www.facebook.com/oskar.pawliszak.5/">
+							<FaSquareFacebook />
+						</a>
+					</div>
+					<div className={classes.social}>
+						<a href="https://www.instagram.com/pawliszakk">
+							<SiInstagram />
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default About;
