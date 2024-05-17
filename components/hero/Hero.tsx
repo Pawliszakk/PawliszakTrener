2f26bdb3b0621435f884684214d3300ac216c93f
Pawliszakk <oskar.pawliszak1@o2.pl>
2024-05-17 12:36:54 +0200

    added typewritter fixed navbar

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,14 +1,31 @@
+'use client';
 import Button from '../UI/Button';
 import classes from './Hero.module.scss';
-
+import { Typewriter } from 'react-simple-typewriter';
 const Hero = () => {
+	const typeWriterWords = [
+		'chcesz wycisnąć pierwszą stówkę?',
+		'chcesz zmienić swoją sylwetkę?',
+		'chcesz poprawić swoją postawę?',
+		'chcesz być sprawniejszy?',
+		'chcesz mieć lepszą kondycję?',
+		'chcesz schudnąć?',
+		'chcesz zyskać pewność siebie?',
+	];
+
 	return (
-		<header className={classes.hero} id='home'>
+		<header className={classes.hero} id="home">
 			<div className={classes.content}>
 				<h1>Pawliszak Trener</h1>
 
 				<div className={classes.typewritter}>
-					<span>CHCESZ BYĆ SPRAWNY?</span>
+					<Typewriter
+						loop={10}
+						typeSpeed={100}
+						cursor
+						delaySpeed={2000}
+						words={typeWriterWords}
+					/>
 				</div>
 
 				<div className={classes.buttons}>
